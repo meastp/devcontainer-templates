e93ab6566a2878a652ab72c5d78cd1643a8e9f4a
Mats Taraldsvik <mats.taraldsvik@gmail.com>
2023-08-15 11:57:19 +0200

    Update test.sh

--- test/cpp/test.sh
+++ test/cpp/test.sh
@@ -5,6 +5,7 @@ source test-utils.sh
 # Template specific tests
 check "distro" lsb_release -c
 check "gcc" gcc -v
+check "g++" g++ -v
 check "greeting" [ $(cat /usr/local/etc/greeting.txt | grep hey) ]
 
 # Report result
