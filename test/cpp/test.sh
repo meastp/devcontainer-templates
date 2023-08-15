34065855818cf315b0be66ac3cff20f9af026861
Mats Taraldsvik <mattar@norkart.no>
2023-08-15 14:35:29 +0200

    temp user

--- test/cpp/test.sh
+++ test/cpp/test.sh
@@ -6,7 +6,6 @@ source test-utils.sh
 check "distro" lsb_release -c
 check "gcc" gcc -v
 check "g++" g++ -v
-check "greeting" [ $(cat /usr/local/etc/greeting.txt | grep hey) ]
 
 # Report result
 reportResults
